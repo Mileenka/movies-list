a97e66813fbef3461221c4655959267514d917b0
Milena Wasowicz <milena.wasowicz@gmail.com>
2023-09-06 15:39:31 +0200

    update

--- src/handlers/addMovieHandler.js
+++ src/handlers/addMovieHandler.js
@@ -1,12 +1,12 @@
 import addMovie from '../../apis/addMovie.js';
 import dom from '../dom.js';
 
-const addMovieHandler = () => {
+const addMovieHandler = async () => {
     const title = dom.movieTitle.value;
     const src = dom.movieSrc.value;
 
     if(title.length >= 1 && src.length >= 5) {
-        addMovie(title, src);
+       await addMovie(title, src);
     }
 
 }
